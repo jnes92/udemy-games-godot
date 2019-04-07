4a7c9a323b163eca9bff845265d197923ae1ba4d
Joesterle <jonas.oesterle@gmx.de>
2019-04-07 10:36:43 +0200

    [04] 59: Adding a simple door with open functions, including sound and animation

---
+++ 3_Heist_Meisters/Scripts/Door.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+var is_player_in_range = false
+
+func _on_Door_body_entered(body):
+	if not body == Global.Player and not $AnimationPlayer.is_playing():
+		open_door()
+	else:
+		is_player_in_range = true
+
+
+func _on_Door_body_exited(body):
+	if body == Global.Player:
+		is_player_in_range = false
+
+
+func open_door():
+	$AnimationPlayer.play("open")
+	
+func _input_event(viewport, event, shape_idx):
+	if Input.is_mouse_button_pressed(BUTTON_LEFT) and is_player_in_range:
+		open_door()
