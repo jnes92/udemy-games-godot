099e8f4113b9b33f6da959db79e6bf7c74a04fed
Jonas Österle <jonas.oesterle@gmx.de>
2019-04-19 18:27:18 +0200

    [04] 59: Fixing input event to more generic one, after removing collision mask

--- 3_Heist_Meisters/Scripts/Door.gd
+++ 3_Heist_Meisters/Scripts/Door.gd
@@ -17,6 +17,6 @@ func _on_Door_body_exited(body):
 func open_door():
 	$AnimationPlayer.play("open")
 	
-func _input_event(viewport, event, shape_idx):
+func _input(event):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and is_player_in_range:
 		open_door()
